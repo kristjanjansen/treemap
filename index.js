aac81f2c0512b45bee17f43eec91a8ce8aeb8f5c
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-25 11:01:45 +0200

    update

--- index.js
+++ index.js
@@ -1,66 +1,24 @@
 import { components } from "https://designstem.github.io/fachwerk/fachwerk.js";
 Vue.component("FEditor", components.FEditor);
 
+import yaml from './yaml.js'
+
 new Vue({
   el: "#app",
   data: {
-    size: 500,
-    yaml: `title: oppekava
-children:
-- title: Yldopingud
-  children:
-  - title: eesti
-    value: 6
-  - title: inglise
-    value: 4.5
-  - title: matem
-    value: 5
-  - title: loodus
-    value: 6
-  - title: sotsiaal
-    value: 7
-  - title: kunst
-    value: 1.5
-- title: Pohiopingud
-  children: 
-  - title: praktika
-    value: 38
-  - title: moodulid
-    children: 
-    - title: alusteadmised
-      value: 9
-    - title: prog. alused
-      value: 10.5
-    - title: arendusprotsess
-      value: 6
-    - title: agiilne
-      value: 4.5
-    - title: andmebaasid
-      value: 7.5
-    - title: veebirakendused
-      value: 9
-    - title: testimine
-      value: 6
-    - title: programmeerimine
-      value: 13.5
-    - title: hajusrakendused
-      value: 6
-    - title: it juhtimine
-      value: 4.5
-    - title: karjaar/ettevotlus
-      value: 6
-- title: valikopingud
-  value: 28`
+    height: 600,
+    width: 1400,
+    yaml
   },
   computed: {
     nodes() {
       return this.yaml
         ? d3
             .treemap()
-            .tile(d3.treemapBinary)
-            .size([this.size * 1.5, this.size])
-            .padding(2)
-            .paddingTop(30)
+            .tile(d3.treemapSquarify.ratio(2))
+            .size([this.width, this.height])
+            .padding(3)
+            .paddingTop(25)
             (d3.hierarchy(this.parsedYaml).sum(d => d.value))
             .descendants()
         : [];
@@ -71,17 +29,20 @@ children:
   },
   methods: {
     color(i) {
-      return this.nodes.length ? d3.interpolateRdYlBu((i / this.nodes.length / 4) + 0.25) : ''
+      return this.nodes.length ? d3.interpolateRdYlBu((i / this.nodes.length / 3) + 0.25) : ''
     }
   },
   template: `
-  <div style="display: flex;">
-  <div style="width: 450px;">
+  <div style="display: flex; height: 100vh;" :style="{width: width + 'px'}">
+  <div style="width: 400px;" v-if="false">
     <f-editor v-model="yaml" style="height: 100vh" />
   </div>
   <div>
-  <svg :width="size * 1.5" height="size">
-    <g v-for="(n,i) in nodes">
+  <svg :width="width" height="height">
+    <g
+      v-for="(n,i) in nodes"
+      :opacity="n.data.title == 'inglise' ? 0.25 : 1"
+    >
       <rect
         :x="n.x0"
         :y="n.y0"
@@ -91,20 +52,21 @@ children:
         :fill="n.depth > 0 ? color(i) : 'none'"
         :rx="5 / n.depth + 1"
         :ry="5 / n.depth + 1"
+        stroke="rgba(0,0,0,0.1)"
       />
       <text
         :x="n.x0 + 5"
-        :y="n.y0 + 5"
+        :y="n.y0 + 3"
         alignment-baseline="text-before-edge"
-        style="font-size: 10px;"
+        style="font-size: 9px;"
         v-html="n.data.title"
       />
       <text
         :x="n.x1 - 5"
-        :y="n.y0 + 5"
+        :y="n.y0 + 3"
         alignment-baseline="text-before-edge"
         text-anchor="end"
-        style="font-size: 10px;"
+        style="font-size: 9px;"
         opacity="0.3"
         v-html="n.value"
       />
---
+++ yaml.js
@@ -0,0 +1,138 @@
+export default `title: oppekava
+children:
+- title: Yldopingud
+  children:
+  - title: eesti
+    value: 6
+  - title: inglise
+    value: 4.5
+  - title: matem
+    value: 5
+  - title: loodus
+    value: 6
+  - title: sotsiaal
+    value: 7
+  - title: kunst
+    value: 1.5
+- title: Pohiopingud
+  children: 
+  - title: praktika
+    value: 38
+  - title: moodulid
+    children: 
+    - title: alusteadmised
+      children:
+      - title: rakendustarkv
+        value: 2
+      - title: arvud.stt
+        value: 1
+      - title: riistvara
+        value: 1.5
+      - title: arvutivorgud
+        value: 1
+      - title: eesti
+        value: 1
+      - title: inglise
+        value: 2.5
+    - title: prog. alused
+      children:
+        - title: dokumenteerimine
+          value: 1
+        - title: rakendusmat
+          value: 3.5
+        - title: progkeeled
+          value: 4
+        - title: OOP
+          value: 1
+        - title: inglise
+          value: 1
+    - title: arendusprotsess
+      children:
+        - title: eesti
+          value: 0.75
+        - title: projektijuht
+          value: 1.5
+        - title: CASE
+          value: 3
+        - title: inglise
+          value: 0.75
+    - title: agiilne
+      children: 
+        - title: klienditeenindus
+          value: 1.5
+        - title: scrum
+          value: 2
+        - title: inglise
+          value: 1
+    - title: andmebaasid
+      children:
+        - title: andmebaasid
+          value: 5
+        - title: loogika
+          value: 1
+        - title: inglise
+          value: 1.5
+    - title: veebirakendused
+      children:
+        - title: stat
+          value: 1
+        - title: tabelid
+          value: 1
+        - title: veebirakendused
+          value: 3
+        - title: html/css
+          value: 1.5
+        - title: java
+          value: 1.5
+        - title: inglise
+          value: 1
+    - title: testimine
+      children:
+      - title: testimine
+        value: 6
+      - title: inglise
+        value: 1.5
+    - title: programmeerimine
+      children:
+      - title: funktsioonid
+        value: 1.5
+      - title: OOP
+        value: 7.5
+      - title: ORM
+        value: 1.5
+      - title: dok.
+        value: 1
+      - title: inglise
+        value: 2
+    - title: hajusrakendused
+      value: 6
+    - title: it juhtimine
+      children:
+      - title: seminar
+        value: 1.5
+      - title: oigus
+        value: 0.5
+      - title: org/juhtimine
+        value: 1
+      - title: infosysteem/taristu
+        value: 1
+      - title: projjuhtimine
+        value: 0.5
+      - title: kliendit
+        value: 1
+      - title: inglise
+        value: 0.5
+    - title: karjaar
+      children:
+      - title: karjaariopetus
+        value: 1
+      - title: majandus/ev
+        value: 1.5
+      - title: toooigus
+        value: 1
+      - title: tookeskkond
+        value: 1
+      - title: suhtlemine
+        value: 1.5
+- title: valikopingud
+  value: 28`
